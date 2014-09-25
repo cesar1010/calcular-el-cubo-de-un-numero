112d2a67f36d4db9bdff91c608538169ab6a118b
César Andrés Rondón Chacón <cesarandresrondonch@hotmail.com>
2014-09-24 19:47:49 -0500

    Create programa-que-calcule-el-cubo.de-un-numerog.cpp
    programa que calcula el cuadrado de un numero

---
+++ programa-que-calcule-el-cubo.de-un-numerog.cpp
@@ -0,0 +1,19 @@
+/*
+REALIZAR UN PROGRAMA QUE CALCULE EL CUBO DE UN NUMERO
+*/
+#include<iostream>
+using namespace std;
+int numero; //
+int cubo; //
+
+int main(){
+cout<<"introduzca el numero q se le calcule el cubo del numero"<<endl;
+cin>>numero;
+//
+cubo = numero * numero * numero;
+
+cout<<cubo<<endl;
+
+system("PAUSE");
+  return 0;  
+    }
